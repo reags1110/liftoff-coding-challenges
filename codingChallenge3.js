a6eafd5a41a74354b65e9c80936c9b0b85a319ac
reags1110 <84357221+reags1110@users.noreply.github.com>
2022-04-25 14:11:36 -0500

    Finished Coding Challenge 3

---
+++ codingChallenge3.js
@@ -0,0 +1,9 @@
+
+
+function FirstReverse(string) {
+    // code goes here 
+    return string.split('').reverse().join('');
+}
+const sample = 'Hello World';
+// keep this function call here 
+console.log (FirstReverse(sample));
